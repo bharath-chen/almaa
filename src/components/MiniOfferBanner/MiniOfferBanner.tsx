a84e7a96443eda87194ba54c39613c3f3cf93151
bharath-chen <sscbarath@gmail.com>
2024-03-07 23:17:54 +0530

    added bg color for dark mode

--- src/components/MiniOfferBanner/MiniOfferBanner.tsx
+++ src/components/MiniOfferBanner/MiniOfferBanner.tsx
@@ -8,10 +8,12 @@ interface Props {
 const MiniOfferBanner = () => {
   return (
     <div className="sticky top-0 w-full z-40">
-      <div className="flex items-center justify-center bg-primary-900 text-white">
+      <div className="flex items-center justify-center bg-primary-900 text-white dark:bg-white dark:text-slate-900">
         <p className="py-2 px-3 font-medium text-xs sm:text-base">
           Welcome to Almaa Herbal Nature's "Health Hub".{" "}
-          <span className={`${styles.highlightText} font-semibold`}>
+          <span
+            className={`${styles.highlightText} font-semibold dark:text-primary-900`}
+          >
             Get 10% Offer on 1st Purchase{" "}
           </span>
         </p>
