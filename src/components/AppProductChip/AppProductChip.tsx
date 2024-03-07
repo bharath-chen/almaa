890380aadd3fa78bd5a3a9608ac1c7d6fc10b880
bharath-chen <sscbarath@gmail.com>
2024-03-07 16:58:21 +0530

    dark mode colors been added

--- src/components/AppProductChip/AppProductChip.tsx
+++ src/components/AppProductChip/AppProductChip.tsx
@@ -12,17 +12,21 @@ const AppProductChip = ({
   onItemChange,
 }: Props) => {
   return (
-    <div>
+    <div className="my-2">
       <h4 className="text-sm font-semibold">{label}</h4>
-      <ul className="flex gap-3 my-3">
+      <ul className="flex flex-wrap gap-2 mb-3">
         {items.map((item) => (
-          <li key={item.id} onClick={() => onItemChange(item)}>
+          <li
+            className="mr-2 mt-4"
+            key={item.id}
+            onClick={() => onItemChange(item)}
+          >
             <span
               className={
-                "cursor-pointer border-2 border-primary-900 text-md font-medium me-2 px-3.5 py-1 rounded-full dark:bg-green-900 dark:text-green-300 " +
+                "cursor-pointer border-2 border-primary-900 dark:border-white text-md font-medium px-3.5 py-1 rounded-full " +
                 (item.id === selectedItem.id
-                  ? "bg-primary-900 text-white"
-                  : "bg-white-100 text-primary-800")
+                  ? "bg-primary-900 text-white dark:bg-white dark:text-slate-900"
+                  : "bg-white-100 text-primary-800 dark:bg-slate-900 dark:text-white")
               }
             >
               {item.label}
