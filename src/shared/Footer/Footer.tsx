876d30c5e0dd2c9a98287175bae331b9adc1757a
bharath-chen <sscbarath@gmail.com>
2024-03-12 01:31:36 +0530

    added colors for footer and changed footer background

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -58,12 +58,12 @@ const widgetMenus: WidgetFooterMenu[] = [
               type="text"
               id="floating_helper"
               aria-describedby="floating_helper_text"
-              className="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-primary-50 dark:bg-neutral-900 border-0 border-b-2 border-primary-700 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary-500 dark:focus:outline-none focus:outline-none focus:ring-0 focus:border-primary-700 peer"
+              className="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-neutral-550 dark:bg-neutral-900 border-0 border-b-2 border-neutral-550 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-neutral-500 dark:focus:outline-none focus:outline-none focus:ring-0 focus:border-neutral-550 peer"
               placeholder=" "
             />
             <label
               htmlFor="floating_helper"
-              className="absolute text-xs text-primary-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-1 peer-focus:text-primary-700 peer-focus:dark:text-primary-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+              className="absolute text-xs text-neutral-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-1 peer-focus:text-neutral-700 peer-focus:dark:text-neutral-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
             >
               Enter your email
             </label>
@@ -86,11 +86,11 @@ const widgetMenus: WidgetFooterMenu[] = [
         href: "#",
         content: () => (
           <>
-            <p className="cursor-default text-xs text-primary-700 dark:text-neutral-300 hover:text-primary-700 dark:hover:text-white">
+            <p className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
               Call Us:{" "}
               <strong className="cursor-pointer">1-800-208-1930</strong>
             </p>
-            <p className="cursor-default text-xs text-primary-700 dark:text-neutral-300 hover:text-primary-700 dark:hover:text-white">
+            <p className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
               Mon-Fri: 9:00am - 5:00pm
             </p>
           </>
@@ -99,7 +99,7 @@ const widgetMenus: WidgetFooterMenu[] = [
       {
         href: "#",
         content: () => (
-          <a className="cursor-default text-xs text-primary-700 dark:text-neutral-300 hover:text-primary-700 dark:hover:text-white">
+          <a className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
             Email Us:{" "}
             <strong className="cursor-pointer">
               contactus@himalayawellness.com
@@ -110,7 +110,7 @@ const widgetMenus: WidgetFooterMenu[] = [
       {
         href: "#",
         content: () => (
-          <a className="cursor-default text-xs text-primary-700 dark:text-neutral-300 hover:text-primary-700 dark:hover:text-white">
+          <a className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
             WhatsApp Us: <strong className="cursor-pointer">89518 91930</strong>
           </a>
         ),
@@ -118,7 +118,7 @@ const widgetMenus: WidgetFooterMenu[] = [
       {
         href: "#",
         content: () => (
-          <a className="text-xs text-primary-700 dark:text-neutral-300 hover:text-primary-700 dark:hover:text-white">
+          <a className="text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
             <strong className="cursor-pointer">
               Report Product Related Issues
             </strong>
@@ -133,7 +133,7 @@ const Footer: React.FC = () => {
   const renderWidgetMenuItem = (menu: WidgetFooterMenu, index: number) => {
     return (
       <div key={index} className={menu.classNames || ""}>
-        <h2 className="text-sm tracking-widest uppercase font-semibold text-primary-700 dark:text-neutral-200">
+        <h2 className="text-sm tracking-widest uppercase font-semibold text-white dark:text-neutral-200">
           {menu.title}
         </h2>
         <ul className="mt-5 space-y-4">
@@ -142,7 +142,7 @@ const Footer: React.FC = () => {
               {item.label && (
                 <a
                   key={index}
-                  className="text-xs text-primary-700 dark:text-neutral-300 hover:text-primary-700 dark:hover:text-white"
+                  className="text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white"
                   href={item.href}
                   target="_blank"
                   rel="noopener noreferrer"
@@ -160,7 +160,7 @@ const Footer: React.FC = () => {
   };
 
   return (
-    <div className="nc-Footer relative py-5 lg:pt-5 lg:pb-5 border-t border-neutral-200 dark:border-neutral-700 bg-primary-50 dark:bg-neutral-900">
+    <div className="nc-Footer relative py-5 lg:pt-5 lg:pb-5 border-t border-neutral-200 dark:border-neutral-700 bg-dark-900 dark:bg-neutral-900">
       {/* <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 "> */}
       <div className="container">
         <div className="flex flex-wrap md:flex-nowrap flex-row gap-y-10 gap-x-10">
