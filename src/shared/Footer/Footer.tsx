b7a01b23564410ac9134259cd6b40f25da74dbf0
bharath-chen <31352595+bharath-chen@users.noreply.github.com>
2024-03-12 12:57:41 +0530

    Update Footer.tsx
    completed the footer design based on original site color codes and font colors

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -14,7 +14,7 @@ const widgetMenus: WidgetFooterMenu[] = [
   {
     id: "5",
     title: "Quick Links",
-    classNames: "basis-2/4 md:basis-1/4",
+    classNames: "lg:col-span-2",
     menus: [
       { href: "#", label: "Ask the Experts" },
       { href: "#", label: "Blog" },
@@ -33,7 +33,7 @@ const widgetMenus: WidgetFooterMenu[] = [
   {
     id: "1",
     title: "Policies",
-    classNames: "md:basis-1/4",
+    classNames: "lg:col-span-2",
     menus: [
       { href: "#", label: "Shipping" },
       { href: "#", label: "Returns & Cancellation" },
@@ -44,7 +44,7 @@ const widgetMenus: WidgetFooterMenu[] = [
   {
     id: "2",
     title: "Subscribe to our newsletter",
-    classNames: "basis-4/4 md:basis-2/4",
+    classNames: "lg:col-span-2",
     menus: [
       {
         href: "#",
@@ -53,20 +53,28 @@ const widgetMenus: WidgetFooterMenu[] = [
       },
       {
         content: () => (
-          <div className="relative">
-            <input
-              type="text"
-              id="floating_helper"
-              aria-describedby="floating_helper_text"
-              className="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-neutral-550 dark:bg-neutral-900 border-0 border-b-2 border-neutral-550 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-neutral-500 dark:focus:outline-none focus:outline-none focus:ring-0 focus:border-neutral-550 peer"
-              placeholder=" "
-            />
-            <label
-              htmlFor="floating_helper"
-              className="absolute text-xs text-neutral-700 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-1 peer-focus:text-neutral-700 peer-focus:dark:text-neutral-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
+            <div className="relative lg:col-span-2">
+              <input
+                type="text"
+                id="floating_helper"
+                aria-describedby="floating_helper_text"
+                className="block px-2.5 pb-2.5 pt-5 w-full text-sm xl:text-md text-neutral-550 bg-dark-900 dark:bg-neutral-900 border-t-2 border-b-2 border-l-2 border-r-2 lg:border-r-0 border-neutral-550 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-neutral-500 dark:focus:outline-none focus:outline-none focus:ring-0 focus:border-neutral-550 peer"
+                placeholder=" "
+              />
+              <label
+                htmlFor="floating_helper"
+                className="absolute text-sm xl:text-md text-neutral-550 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 left-[9px] z-10 origin-[0] start-1 peer-focus:text-neutral-700 peer-focus:dark:text-neutral-700 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+              >
+                Enter your email
+              </label>
+            </div>
+            <button
+              type="button"
+              className="mt-2 lg:mt-0 focus:outline-none h-full text-white bg-greenyellow-900 hover:bg-greenyellow-900 focus:ring-0 focus:ring-greenyellow-900 font-medium text-sm xl:text-md px-5 py-2.5 mb-2 dark:bg-greenyellow-900 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
-              Enter your email
-            </label>
+              Submit
+            </button>
           </div>
         ),
       },
@@ -76,7 +84,7 @@ const widgetMenus: WidgetFooterMenu[] = [
   {
     id: "4",
     title: "Contact Us",
-    classNames: "basis-4/4 md:basis-2/4",
+    classNames: "lg:col-span-2",
     menus: [
       {
         href: "#",
@@ -86,11 +94,11 @@ const widgetMenus: WidgetFooterMenu[] = [
         href: "#",
         content: () => (
           <>
-            <p className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
+            <p className="cursor-default text-sm xl:text-md text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
               Call Us:{" "}
               <strong className="cursor-pointer">1-800-208-1930</strong>
             </p>
-            <p className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
+            <p className="cursor-default text-sm xl:text-md text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
               Mon-Fri: 9:00am - 5:00pm
             </p>
           </>
@@ -99,7 +107,7 @@ const widgetMenus: WidgetFooterMenu[] = [
       {
         href: "#",
         content: () => (
-          <a className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
+          <a className="cursor-default text-sm xl:text-md text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
             Email Us:{" "}
             <strong className="cursor-pointer">
               contactus@himalayawellness.com
@@ -110,7 +118,7 @@ const widgetMenus: WidgetFooterMenu[] = [
       {
         href: "#",
         content: () => (
-          <a className="cursor-default text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
+          <a className="cursor-default text-sm xl:text-md text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
             WhatsApp Us: <strong className="cursor-pointer">89518 91930</strong>
           </a>
         ),
@@ -118,7 +126,7 @@ const widgetMenus: WidgetFooterMenu[] = [
       {
         href: "#",
         content: () => (
-          <a className="text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
+          <a className="text-sm xl:text-md text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white">
             <strong className="cursor-pointer">
               Report Product Related Issues
             </strong>
@@ -133,7 +141,7 @@ const Footer: React.FC = () => {
   const renderWidgetMenuItem = (menu: WidgetFooterMenu, index: number) => {
     return (
       <div key={index} className={menu.classNames || ""}>
-        <h2 className="text-sm tracking-widest uppercase font-semibold text-white dark:text-neutral-200">
+        <h2 className="text-base xl:text-lg 2xl:text-xl tracking-widest uppercase font-semibold text-white dark:text-neutral-200">
           {menu.title}
         </h2>
         <ul className="mt-5 space-y-4">
@@ -142,7 +150,7 @@ const Footer: React.FC = () => {
               {item.label && (
                 <a
                   key={index}
-                  className="text-xs text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white"
+                  className="text-sm xl:text-md text-neutral-550 dark:text-neutral-300 hover:text-neutral-550 dark:hover:text-white"
                   href={item.href}
                   target="_blank"
                   rel="noopener noreferrer"
@@ -163,7 +171,7 @@ const Footer: React.FC = () => {
     <div className="nc-Footer relative py-5 lg:pt-5 lg:pb-5 border-t border-neutral-200 dark:border-neutral-700 bg-dark-900 dark:bg-neutral-900">
       {/* <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 "> */}
       <div className="container">
-        <div className="flex flex-wrap md:flex-nowrap flex-row gap-y-10 gap-x-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-10">
           {widgetMenus.map(renderWidgetMenuItem)}
         </div>
         {/* <div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
