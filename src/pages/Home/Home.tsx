cc688f86b105528619e3ef4b229c799c9ab7a37e
bharath-chen <sscbarath@gmail.com>
2024-04-11 22:22:07 +0530

    used imported home data in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,10 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import heroImg from "../../assets/HOME PAGE/1-slider-image.jpg";
 import ButtonSecondary from "../../shared/Button/ButtonSecondary";
 import aboutSectionImg from "../../assets/HOME PAGE/2-about-section.jpg";
-import img1 from "../../assets/HOME PAGE/3-carousal-1.png";
-import img2 from "../../assets/HOME PAGE/3-carousal-2.png";
-import img3 from "../../assets/HOME PAGE/3-carousal-3.png";
-import img4 from "../../assets/HOME PAGE/3-carousal-4.png";
-import categoryImg1 from "../../assets/HOME PAGE/8-carousal-1.png";
-import categoryImg2 from "../../assets/HOME PAGE/8-carousal-2.png";
-import categoryImg3 from "../../assets/HOME PAGE/8-carousal-3.png";
-import categoryImg4 from "../../assets/HOME PAGE/8-carousal-4.png";
 import whyAlmaaImg from "../../assets/HOME PAGE/10-why-section.jpg";
 import certification1Img from "../../assets/HOME PAGE/11-certificates.jpg";
-import { useEffect, useState } from "react";
 import Heading from "../../components/Heading/Heading";
 import CardCategory3 from "../../components/CardCategories/CardCategory3";
 import AccordionInfo from "../../containers/ProductDetailPage/AccordionInfo";
@@ -21,17 +13,7 @@ import SectionClientSay from "../../components/SectionClientSay/SectionClientSay
 import BackgroundSection from "../../components/BackgroundSection/BackgroundSection";
 import SectionMagazine5 from "../../containers/BlogPage/SectionMagazine5";
 import SectionSliderCategories from "../../components/SectionSliderCategories/SectionSliderCategories";
-import medicalConsultant1 from "../../assets/HOME PAGE/7-doctor-1.jpg";
-import medicalConsultant2 from "../../assets/HOME PAGE/7-doctor-2.jpg";
-import medicalConsultant3 from "../../assets/HOME PAGE/7-doctor-3.jpg";
-import medicalConsultant4 from "../../assets/HOME PAGE/7-doctor-4.jpg";
 import SectionGridMoreExplore from "../../components/SectionGridMoreExplore/SectionGridMoreExplore";
-import exploreImg1 from "../../assets/HOME PAGE/6-explore-1.png";
-import exploreImg2 from "../../assets/HOME PAGE/6-explore-2.png";
-import exploreImg3 from "../../assets/HOME PAGE/6-explore-3.png";
-import exploreImg4 from "../../assets/HOME PAGE/6-explore-4.png";
-import exploreImg5 from "../../assets/HOME PAGE/6-explore-5.png";
-import exploreImg6 from "../../assets/HOME PAGE/6-explore-6.png";
 import Button from "../../shared/Button/Button";
 import mdSectionImg from "../../assets/HOME PAGE/5-md-section.png";
 import Nav from "../../shared/Nav/Nav";
@@ -39,289 +21,22 @@ import NavItem from "../../shared/NavItem/NavItem";
 import productsService from "../../services/products-service";
 import ProductCard from "../../components/ProductCard";
 import emailSubscribeImg from "../../assets/HOME PAGE/14-subscribe.png";
-import video1Img from "../../assets/HOME PAGE/9-video-1.jpg";
-import video2Img from "../../assets/HOME PAGE/9-video-2.jpg";
-import video3Img from "../../assets/HOME PAGE/9-video-3.jpg";
 import saveAndExploreImg from "../../assets/HOME PAGE/12-save-section.png";
 import AppSlider from "../../components/AppSlider/AppSlider";
 import { Product } from "../../data/data";
+import {
+  ABOUTS,
+  MEDIC_SLIDERS,
+  CATEGORY_SLIDERS,
+  ACCORDION_INFO,
+  MEDICAL_CONSULTANTS,
+  TABS,
+  EXPLORE_SECTION_DATA,
+  LIFE_STYLE_CARDS,
+} from "../../data/home";
 
 const Home = () => {
   const navigate = useNavigate();
-
-  const abouts = [
-    {
-      id: 1,
-      label: "25+ Doctors",
-      descripiton: "Experienced and multispecialised doctors",
-    },
-    {
-      id: 2,
-      label: "300+ Products",
-      descripiton: "100% natural products with herbal ingredients",
-    },
-    {
-      id: 3,
-      label: "15+ Years",
-      descripiton: "Pioneer in siddha industry with expretise in products",
-    },
-  ];
-
-  const medicSliders = [
-    {
-      name: "Enhance your",
-      desc: "Respiratory <br/> Wellbeing",
-      featuredImage: img1,
-      color: "bg-pink-50",
-    },
-    {
-      name: "Take care of",
-      desc: "Digestive <br /> Wellbeing",
-      featuredImage: img2,
-      color: "bg-red-50",
-    },
-    {
-      name: "Enhance your",
-      desc: "Reproductive <br /> Wellbeing",
-      featuredImage: img3,
-      color: "bg-green-50",
-    },
-    {
-      name: "Enhance your",
-      desc: "Blood Pressure <br /> Wellbeing",
-      featuredImage: img4,
-      color: "bg-orange-50",
-    },
-  ];
-
-  const categorySliders = [
-    {
-      name: "Best in class",
-      desc: "Herbal <br/> Powders",
-      featuredImage: categoryImg1,
-      color: "bg-slate-50",
-    },
-    {
-      name: "Pure & Quality",
-      desc: "Chooranams",
-      featuredImage: categoryImg2,
-      color: "bg-slate-50",
-    },
-    {
-      name: "Thick & Consistent",
-      desc: "Syrup",
-      featuredImage: categoryImg3,
-      color: "bg-slate-50",
-    },
-    {
-      name: "Tablets",
-      desc: "Herbal Tablets",
-      featuredImage: categoryImg4,
-      color: "bg-slate-50",
-    },
-  ];
-
-  const accordionInfo = [
-    {
-      name: "Enhanced Lifestyle",
-      content:
-        "Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.",
-    },
-    {
-      name: "Taking care of your whole family",
-      content: `<ul class="list-disc list-inside leading-7">
-      <li>Made from a sheer Belgian power micromesh.</li>
-      <li>
-      74% Polyamide (Nylon) 26% Elastane (Spandex)
-      </li>
-      <li>
-      Adjustable hook & eye closure and straps
-      </li>
-      <li>
-      Hand wash in cold water, dry flat
-      </li>
-    </ul>`,
-    },
-    {
-      name: "Genuine and perfect health care solutions",
-      content:
-        "Use this as a guide. Preference is a huge factor — if you're near the top of a size range and/or prefer more coverage, you may want to size up.",
-    },
-    {
-      name: "Best in industry products at high quality and own manufacturing to ensure quality of products",
-      content: `
-      <ul class="list-disc list-inside leading-7">
-      <li>All full-priced, unworn items, with tags attached and in their original packaging are eligible for return or exchange within 30 days of placing your order.</li>
-      <li>
-      Please note, packs must be returned in full. We do not accept partial returns of packs.
-      </li>
-      <li>
-      Want to know our full returns policies? Here you go.
-      </li>
-      <li>
-      Want more info about shipping, materials or care instructions? Here!
-      </li>
-    </ul>
-      `,
-    },
-    {
-      name: "Spiritual touch which gives 100% solutions",
-      content: `
-      <ul class="list-disc list-inside leading-7">
-      <li>All full-priced, unworn items, with tags attached and in their original packaging are eligible for return or exchange within 30 days of placing your order.</li>
-      <li>
-      Please note, packs must be returned in full. We do not accept partial returns of packs.
-      </li>
-      <li>
-      Want to know our full returns policies? Here you go.
-      </li>
-      <li>
-      Want more info about shipping, materials or care instructions? Here!
-      </li>
-    </ul>
-      `,
-    },
-  ];
-
-  const medicalConsultants = [
-    {
-      name: "Dr.Manikandan B.A.M.S",
-      desc: "Ortho Specialist",
-      img: medicalConsultant1,
-      color: "",
-    },
-    {
-      name: "Dr.Sowmya B.A.M.S",
-      desc: "Varma Specialist",
-      img: medicalConsultant2,
-      color: "",
-    },
-    {
-      name: "Dr.Kumaravel M.D(s).",
-      desc: "Senior Consultant (General)",
-      img: medicalConsultant3,
-      color: "",
-    },
-    {
-      name: "Dr.Sankar Anand M.D(s)",
-      desc: "Doctor, General Medicine",
-      img: medicalConsultant4,
-      color: "",
-    },
-  ];
-
-  const exploreSectionData = [
-    {
-      id: 1,
-      name: "Talk to Doctor",
-      desc: "Free Consulting",
-      image: exploreImg1,
-      svgBg: "",
-      color: "bg-indigo-50",
-      btnLabel: "Explore Doctors",
-      quantityText: "25 Doctors",
-    },
-    {
-      id: 2,
-      name: "Wellness theraphy",
-      desc: "Varma Treatments",
-      image: exploreImg2,
-      svgBg: "",
-      color: "bg-slate-100/80",
-      btnLabel: "Explore Now",
-      quantityText: "35 Therapies",
-    },
-    {
-      id: 3,
-      name: "Library",
-      desc: "Books & Resource materials",
-      image: exploreImg3,
-      svgBg: "",
-      color: "bg-violet-50",
-      btnLabel: "Explore Now",
-      quantityText: "15 Books",
-    },
-    {
-      id: 4,
-      name: "Siddhar Pooja",
-      desc: "Amavasai & Pournami",
-      image: exploreImg4,
-      svgBg: "",
-      color: "bg-orange-50",
-      btnLabel: "Explore Now",
-      quantityText: "2 Locations",
-    },
-    {
-      id: 5,
-      name: "Production Unit",
-      desc: "We Manufacture We Serve",
-      image: exploreImg5,
-      svgBg: "",
-      color: "bg-blue-50",
-      btnLabel: "Explore Now",
-      quantityText: "200 Products",
-    },
-    {
-      id: 6,
-      name: "For Your Health",
-      desc: "Awarness & Social Responsibility",
-      image: exploreImg6,
-      svgBg: "",
-      color: "bg-orange-50",
-      btnLabel: "Explore Now",
-      quantityText: "500 Resources",
-    },
-    // {
-    //   id: 7,
-    //   name: "Sunglasses",
-    //   desc: "Manufacturar",
-    //   image: explore7Png,
-    //   svgBg: explore7Svg,
-    //   color: "bg-stone-100",
-    // },
-    // {
-    //   id: 8,
-    //   name: "kid hats",
-    //   desc: "Manufacturar",
-    //   image: explore8Png,
-    //   svgBg: explore8Svg,
-    //   color: "bg-blue-50",
-    // },
-    // {
-    //   id: 9,
-    //   name: "Wool Jacket",
-    //   desc: "Manufacturar",
-    //   image: explore4Png,
-    //   svgBg: explore4Svg,
-    //   color: "bg-slate-100/80",
-    // },
-  ];
-
-  const lifeStyleCards = [
-    {
-      id: 1,
-      src: video1Img,
-      heading: "How to enhance your lifestyle in a healthy way ?",
-      categoryType: "LifeStyle",
-      dateAdded: "04<sup>th</sup> Feb 2024",
-    },
-    {
-      id: 2,
-      src: video2Img,
-      heading: "Healthy life using natural & organic ingredients",
-      categoryType: "Food Practice",
-      dateAdded: "02<sup>nd</sup> Feb 2024",
-    },
-    {
-      id: 3,
-      src: video3Img,
-      heading: "Let's elevate your life! Participate in Pournami Pooja...",
-      categoryType: "LifeStyle",
-      dateAdded: "05<sup>th</sup> Feb 2024",
-    },
-  ];
-
-  const tabs = ["Best Sellers", "Offers", "Newly Launched"];
 
   const renderCategoryCard = (item: {
     name: string;
@@ -462,7 +177,7 @@ const Home = () => {
         </div>
 
         <div className="mt-14 grid grid-cols-1 md:grid-cols-3 gap-10">
-          {abouts.map((about) => (
+          {ABOUTS.map((about) => (
             <div className="p-4 bg-slate-100 rounded-lg" key={about.id}>
               <h4 className="text-lg font-semibold mb-1">{about.label}</h4>
               <p className="text-xs md:text-sm text-gray-400">
@@ -477,7 +192,7 @@ const Home = () => {
       <section className="mb-40">
         <AppSlider
           className="nc-DiscoverMoreSlider nc-p-l-container "
-          data={medicSliders}
+          data={MEDIC_SLIDERS}
           renderChildren={renderCategoryCard}
         >
           <Heading
@@ -499,7 +214,7 @@ const Home = () => {
           className="sm:space-x-2"
           containerClassName="relative flex w-full overflow-x-auto text-sm md:text-base hiddenScrollbar"
         >
-          {tabs.map((item, index) => (
+          {TABS.map((item, index) => (
             <NavItem
               key={index}
               isActive={tabActive === item}
@@ -604,7 +319,7 @@ const Home = () => {
         <div className="relative py-24 lg:py-32">
           <BackgroundSection className="bg-neutral-100/70 dark:bg-black/20" />
           <SectionGridMoreExplore
-            data={exploreSectionData}
+            data={EXPLORE_SECTION_DATA}
             // className="bg-neutral-100/70 dark:bg-black/20 rounded-2xl px-16 py-20"
           />
         </div>
@@ -615,7 +330,7 @@ const Home = () => {
         <SectionSliderCategories
           heading="Almaa's"
           rightDescText="Medical Consultants"
-          data={medicalConsultants}
+          data={MEDICAL_CONSULTANTS}
         />
         <ButtonSecondary className="focus:ring-2 focus:ring-offset-2 focus:ring-transparent tracking-tight ml-3 mt-6 md:text-2xl sm:px-14 border sm:py-5 sm:text-dark sm:bg-white-900 sm:hover:bg-white sm:hover:text-primary-900 border border-slate-300 dark:border-slate-700">
           Visit all Doctors
@@ -625,7 +340,7 @@ const Home = () => {
       {/* EXPLORE PRODUCTS BY CATEGORIES SECTION  */}
       <section className="mb-40">
         <AppSlider
-          data={categorySliders}
+          data={CATEGORY_SLIDERS}
           className="nc-DiscoverMoreSlider nc-p-l-container "
           renderChildren={renderCategoryCard}
         >
@@ -651,7 +366,7 @@ const Home = () => {
       {/* HEALTHY AND LIFESTYLE VIDEOS SECTION */}
       <section className="container mb-40">
         <AppSlider
-          data={lifeStyleCards}
+          data={LIFE_STYLE_CARDS}
           className="glidejs_rb_ flow-root glide--swipeable glide--ltr glide--slider"
           glideClassName="glide__track"
           renderChildren={renderHealthAndLifestyleCard}
@@ -707,7 +422,7 @@ const Home = () => {
             />
           </div>
           <div>
-            <AccordionInfo data={accordionInfo} />
+            <AccordionInfo data={ACCORDION_INFO} />
           </div>
         </div>
       </section>
