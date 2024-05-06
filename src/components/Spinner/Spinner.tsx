01ae8c373c777a90e8ee11c998ab4f2b5e7d4055
bharath-chen <sscbarath@gmail.com>
2024-05-06 23:25:04 +0530

    spinner size change

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -22,27 +22,6 @@ const Spinner: React.FC<SpinnerProps> = ({
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      {/* <svg
-        className={`animate-spin ${getSizeClasses()} text-${color}-500`}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 4.418 3.582 8 8 8v-4zm16-1.291A7.962 7.962 0 0120 12h4c0 4.418-3.582 8-8 8v-4z"
-        ></path>
-      </svg> */}
-
       <div role="status">
         <svg
           aria-hidden="true"
