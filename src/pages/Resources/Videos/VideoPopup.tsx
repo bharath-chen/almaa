b56b1439de770aabf6ed11bc95aa17dad924dde9
bharath-chen <sscbarath@gmail.com>
2024-05-08 23:05:02 +0530

    added responsive react video player

--- src/pages/Resources/Videos/VideoPopup.tsx
+++ src/pages/Resources/Videos/VideoPopup.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
 
 interface VideoPopupProps {
@@ -38,14 +37,19 @@ const VideoPopup = ({
                   viewBox="0 0 24 24"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
               </button>
-              <div className="w-full">
-                <ReactPlayer url={url} />
+              <div className="player-wrapper">
+                <ReactPlayer
+                  url={url}
+                  className="react-player"
+                  width="100%"
+                  height="100%"
+                />
               </div>
             </div>
           </div>
