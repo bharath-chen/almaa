a38ba2d70f00401f0646538ba3195845e43da574
bharath-chen <sscbarath@gmail.com>
2024-08-11 13:56:27 +0530

    added provider for redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//
 import "./styles/index.scss";
 import "./index.css";
 import "./fonts/line-awesome-1.3.0/css/line-awesome.css";
 import "rc-slider/assets/index.css";
 
-//
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./state";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
