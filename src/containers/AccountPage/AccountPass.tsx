aca835fcb55ddf51b6225d76a112b9a2b8622698
bharath-chen <sscbarath@gmail.com>
2024-08-12 08:19:13 +0530

    added change password and integrated change password api

--- src/containers/AccountPage/AccountPass.tsx
+++ src/containers/AccountPage/AccountPass.tsx
@@ -1,10 +1,36 @@
 import Label from "../../components/Label/Label";
-import React from "react";
+import { useState } from "react";
 import ButtonPrimary from "../../shared/Button/ButtonPrimary";
 import Input from "../../shared/Input/Input";
 import CommonLayout from "./CommonLayout";
+import updatePasswordService from "../../services/update-password-service";
+import { ILoginCustomerPayload } from "../../services/login-service";
 
 const AccountPass = () => {
+  const [accountPasswordDetails, setAccountPasswordDetails] = useState({
+    currentPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  const handleUpdatePassword = () => {
+    const customerDetails: ILoginCustomerPayload = JSON.parse(
+      localStorage.getItem("customerDetails")
+    );
+    // customer_id=1&password=123456
+    const { request } = updatePasswordService.get<
+      any,
+      { customer_id: number; password: string }
+    >({
+      customer_id: +customerDetails.customer_id,
+      password: accountPasswordDetails.newPassword,
+    });
+
+    request.then((res) => {
+      console.log(res);
+    });
+  };
+
   return (
     <div>
       <CommonLayout>
@@ -16,18 +42,50 @@ const AccountPass = () => {
           <div className=" max-w-xl space-y-6">
             <div>
               <Label>Current password</Label>
-              <Input type="password" className="mt-1.5" />
+              <Input
+                type="password"
+                className="mt-1.5"
+                value={accountPasswordDetails.currentPassword}
+                onChange={(e) =>
+                  setAccountPasswordDetails({
+                    ...accountPasswordDetails,
+                    currentPassword: e.target.value,
+                  })
+                }
+              />
             </div>
             <div>
               <Label>New password</Label>
-              <Input type="password" className="mt-1.5" />
+              <Input
+                type="password"
+                className="mt-1.5"
+                value={accountPasswordDetails.newPassword}
+                onChange={(e) =>
+                  setAccountPasswordDetails({
+                    ...accountPasswordDetails,
+                    newPassword: accountPasswordDetails.newPassword,
+                  })
+                }
+              />
             </div>
             <div>
               <Label>Confirm password</Label>
-              <Input type="password" className="mt-1.5" />
+              <Input
+                type="password"
+                className="mt-1.5"
+                value={accountPasswordDetails.confirmPassword}
+                onChange={(e) =>
+                  setAccountPasswordDetails({
+                    ...accountPasswordDetails,
+                    confirmPassword: e.target.value,
+                  })
+                }
+              />
             </div>
             <div className="pt-2">
-              <ButtonPrimary>Update password</ButtonPrimary>
+              <ButtonPrimary onClick={handleUpdatePassword}>
+                Update password
+              </ButtonPrimary>
             </div>
           </div>
         </div>
