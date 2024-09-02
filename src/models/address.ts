117bdff716d19ee3d62088e28e9e2db620ce3103
bharath-chen <sscbarath@gmail.com>
2024-09-02 10:22:06 +0530

    added address model

---
+++ src/models/address.ts
@@ -0,0 +1,15 @@
+export interface Address {
+  address_id: string;
+  customer_id: string;
+  doorno: string;
+  street: string;
+  location: string;
+  pincode: string;
+  city: string;
+  state: string;
+  primary_use: string;
+  recently_use: string;
+  status: string;
+  created_date: string;
+  updated: string;
+}
