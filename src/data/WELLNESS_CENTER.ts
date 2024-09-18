ceabbb0c1e0a9e808d642b18462e0b1e974f5223
bharath-chen <sscbarath@gmail.com>
2024-09-18 21:06:31 +0530

    faq static content change wellness center

---
+++ src/data/WELLNESS_CENTER.ts
@@ -0,0 +1,61 @@
+export const WELLNESS_CENTER = {
+  faq: [
+    {
+      id: "q1",
+      name: "What is Siddha medicine?",
+      content: `Siddha medicine is a Traditional system of Medicine that originated in Tamilnadu. It focuses on
+balancing the body&#39;s energies through natural remedies, diet, and lifestyle adjustments to treat
+diseases &amp; promote overall health and wellness.`,
+    },
+    {
+      id: "q2",
+      name: "What types of treatments do you offer at the Vela SIddha Village Resort?",
+      content: `We offer a wide range of Siddha treatments including herbal therapies, detox programs,
+rejuvenation therapies, and personalized wellness plans tailored to individual needs.`,
+    },
+    {
+      id: "q3",
+      name: "What Siddha treatments are available for arthritis?",
+      content: `We offer various Siddha treatments for arthritis, including Varmam, Detoxifications and
+specialized dietary plans aimed at reducing inflammation and improving joint function.`,
+    },
+    {
+      id: "q4",
+      name: "Are the treatments suitable for all types of arthritis?",
+      content: `Our treatments can be customized for various types of arthritis, including osteoarthritis and
+rheumatoid arthritis. A detailed consultation will help us tailor the approach to your specific
+condition.`,
+    },
+    {
+      id: "q5",
+      name: "Are there any recreational activities available at Vela Siddha Village Resort?",
+      content: `In addition to our wellness treatments, we offer recreational activities such as nature walks,
+cultural experiences, and workshops to enhance your stay.`,
+    },
+    {
+      id: "q6",
+      name: "What kind of diet will be recommended for diabetes management?",
+      content: `We provide a diet rich in fibre, low in sugar, and balanced in nutrients, tailored to stabilize
+blood sugar levels and support overall health.`,
+    },
+    {
+      id: "q7",
+      name: "What Siddha treatments are available to support fertility?",
+      content: `Our resort offers treatments aimed at improving reproductive health through detoxification,
+herbal remedies, and dietary adjustments that support hormonal balance and overall well-
+being`,
+    },
+    {
+      id: "q8",
+      name: "What kind of diet will I follow during my stay in the resort?",
+      content: `You will follow a diet designed to support weight loss and overall health, which includes
+nutrient-dense foods, balanced macronutrients, and portions tailored to your specific needs.`,
+    },
+    {
+      id: "q9",
+      name: "Will I receive guidance on lifestyle changes to support my well-being?",
+      content: `Our program includes lifestyle recommendations such as stress management techniques, sleep
+improvement, and daily routines that support overall health &amp; well-being.`,
+    },
+  ],
+};
