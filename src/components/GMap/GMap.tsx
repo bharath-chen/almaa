c094763e44841fd60db44df9e23fd4ed255a8b9b
bharath-chen <sscbarath@gmail.com>
2024-09-18 21:06:13 +0530

    GMap content change

--- src/components/GMap/GMap.tsx
+++ src/components/GMap/GMap.tsx
@@ -6,7 +6,7 @@ interface Props {
 const GMap = ({ width = "100%", height = "100%" }: Props) => {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d62317.021057207035!2d78.205329!3d12.52849!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bac359a423ca2bb%3A0x2d48a71cc2782f48!2sAlmaa%20Siddha%20Care%20Clinic!5e0!3m2!1sen!2sin!4v1720879326481!5m2!1sen!2sin"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7774.352396480873!2d80.225044!3d13.024449!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a526655c38d2993%3A0x54b714477f05829b!2sAlmaa%20Siddha%20Care%20Multispeciality%20Hospital!5e0!3m2!1sen!2sus!4v1726594030466!5m2!1sen!2sus"
       allowFullScreen={true}
       style={{
         display: "block",
