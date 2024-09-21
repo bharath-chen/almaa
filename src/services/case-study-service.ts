17d23a620168169f86a8def3cd60820631c756c0
bharath-chen <sscbarath@gmail.com>
2024-09-21 20:19:22 +0530

    case study model change

--- src/services/case-study-service.ts
+++ src/services/case-study-service.ts
@@ -2,11 +2,15 @@ import create from "./http-service";
 
 export interface ICaseStudy {
   case_study_id: string;
+  case_details: string;
   title: string;
+  diagnosis: string;
   description: string;
   case_study_url: string;
+  image_url: string;
   date: null;
   status: string;
+  result: string;
 }
 
 export default create("?gofor=casestudylist");
