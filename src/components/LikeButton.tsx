e9cd933eeb95977835712c4963ef6e249aa84f76
bharath-chen <sscbarath@gmail.com>
2024-10-05 13:13:01 +0530

    removed console

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -22,8 +22,6 @@ const LikeButton: React.FC<LikeButtonProps> = ({
     onLike();
   };
 
-  console.log(isLiked);
-
   return (
     <button
       className={`w-9 h-9 flex items-center justify-center rounded-full bg-white dark:bg-slate-900 text-neutral-700 dark:text-slate-200 nc-shadow-lg ${className}`}
