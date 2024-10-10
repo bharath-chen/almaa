f9b6530e67d77ea982ab761c4b04ff2a730c73b1
bharath-chen <sscbarath@gmail.com>
2024-10-10 22:35:57 +0530

    content change and added CTA link in OfferBanner

--- src/components/MiniOfferBanner/MiniOfferBanner.tsx
+++ src/components/MiniOfferBanner/MiniOfferBanner.tsx
@@ -1,22 +1,20 @@
-import { ReactNode } from "react";
 import styles from "./MiniOfferBanner.module.css";
-
-interface Props {
-  children: ReactNode;
-}
+import { Link } from "react-router-dom";
 
 const MiniOfferBanner = () => {
   return (
     <div className="sticky top-0 w-full z-40">
       <div className="flex items-center justify-center bg-primary-900 text-white dark:bg-white dark:text-slate-900">
-        <p className="py-2 px-3 font-medium text-xs sm:text-base">
-          Welcome to Almaa Herbal Nature's "Health Hub".{" "}
-          <span
-            className={`${styles.highlightText} font-semibold dark:text-primary-900`}
-          >
-            Get 10% Offer on 1st Purchase{" "}
-          </span>
-        </p>
+        <Link to="/products">
+          <p className="py-2 px-3 font-medium text-xs sm:text-base">
+            Welcome to Almaa Siddha official website.{" "}
+            <span
+              className={`${styles.highlightText} font-semibold dark:text-primary-900`}
+            >
+              Boost your health with best Siddha Products .{" "}
+            </span>
+          </p>
+        </Link>
       </div>
     </div>
   );
