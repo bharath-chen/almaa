ff794ee7a1171774670d6ea3c2ac230a52040961
bharath-chen <sscbarath@gmail.com>
2024-10-17 22:42:38 +0530

    removed environments

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  razorPayApiKey: "rzp_live_J4GuuYTXoI4jGW",
-  apiUrl: "https://almaherbal.top/App",
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  razorPayApiKey: "rzp_test_28UhRPu2GtFse3",
-  apiUrl: "https://almaherbal.top/App",
-};
