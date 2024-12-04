de1477cb0039ed994755bb0d4f6334e29e67b54e
bharath-chen <sscbarath@gmail.com>
2024-12-05 00:34:46 +0530

    added dynamic imports

--- src/routers/pages.ts
+++ src/routers/pages.ts
@@ -1,47 +1,67 @@
 import { Page } from "./types";
-import AccountPage from "../containers/AccountPage/AccountPage";
-import PageAbout from "../containers/PageAbout/PageAbout";
-import PageSignUp from "../containers/PageSignUp/PageSignUp";
-import Login from "../pages/Login/Login";
-import BlogSingle from "../containers/BlogPage/BlogSingle";
-import PageSearch from "../containers/PageSearch";
-import ProductDetailPage2 from "../containers/ProductDetailPage/ProductDetailPage2";
-import AccountSavelists from "../containers/AccountPage/AccountSavelists";
-import AccountPass from "../containers/AccountPage/AccountPass";
-import AccountSupport from "../containers/AccountPage/AccountSupport";
-import AccountOrder from "../containers/AccountPage/AccountOrder";
-import CartPage from "../containers/ProductDetailPage/CartPage";
-import CheckoutPage from "../containers/PageCheckout/CheckoutPage";
-import Home from "../pages/Home/Home";
-import Products from "../pages/Products/Products";
-import Page404 from "../containers/Page404/Page404";
-import Blog from "../pages/Resources/Blog/Blog";
-import CaseStudies from "../pages/Resources/CaseStudies/CaseStudies";
-import Support from "../pages/Support/Support";
-import Library from "../pages/Library/Library";
-import WellnessCenter from "../pages/WellnessCenter/WellnessCenter";
-import ProductionUnit from "../pages/ProductionUnit/ProductionUnit";
-import AlmaaGroups from "../pages/About/AlmaaGroups/AlmaaGroups";
-import Founder from "../pages/About/Founder/Founder";
-import DoctorsTeam from "../pages/About/DoctorsTeam/DoctorsTeam";
-import OurBranches from "../pages/About/OurBranches/OurBranches";
-import SiddhaScience from "../pages/About/SiddhaScience/SiddhaScience";
-import OurConcepts from "../pages/About/OurConcepts/OurConcepts";
-import DoctorDetail from "../pages/About/DoctorsTeam/DoctorDetail";
-import NewsAndEvents from "../pages/Resources/NewsAndEvents/NewsAndEvents";
-import Media from "../pages/Resources/Media/Media";
-import Videos from "../pages/Resources/Videos/Videos";
-import CaseStudyDetail from "../pages/Resources/CaseStudies/CaseStudyDetail";
-import Faq from "../pages/Faq";
-import TermsAndConditions from "../pages/TermsAndConditions/TermsAndConditions";
-import PrivacyPolicy from "../pages/PrivacyPolicy/PrivacyPolicy";
-import ForgotPassword from "../pages/ForgotPassword/ForgotPassword";
-import ThanksPage from "../pages/ThanksPage/ThanksPage";
-import Returns from "../pages/Returns/Returns";
-import NewsAndEventsDetail from "../pages/Resources/NewsAndEvents/NewsAndEventsDetail";
-import ShippingPolicy from "../pages/ShippingPolicy/ShippingPolicy";
-import Invoice from "../components/Invoice/Invoice";
-import AccountAddress from "../containers/AccountPage/AccountAddress";
+import { lazyComponent } from "../utils/lazy-utils";
+
+const Home = lazyComponent("../pages/Home/Home");
+const Products = lazyComponent("../pages/Products/Products");
+const ProductDetail = lazyComponent(
+  "../containers/ProductDetailPage/ProductDetailPage2"
+);
+const PageSearch = lazyComponent("../containers/PageSearch");
+const AccountPage = lazyComponent("../containers/AccountPage/AccountPage");
+const AccountSavelists = lazyComponent(
+  "../containers/AccountPage/AccountSavelists"
+);
+const AccountPass = lazyComponent("../containers/AccountPage/AccountPass");
+const AccountSupport = lazyComponent(
+  "../containers/AccountPage/AccountSupport"
+);
+const AccountOrder = lazyComponent("../containers/AccountPage/AccountOrder");
+const AccountAddress = lazyComponent(
+  "../containers/AccountPage/AccountAddress"
+);
+const CartPage = lazyComponent("../containers/ProductDetailPage/CartPage");
+const CheckoutPage = lazyComponent("../containers/PageCheckout/CheckoutPage");
+const ThanksPage = lazyComponent("../pages/ThanksPage/ThanksPage");
+const Blog = lazyComponent("../pages/Resources/Blog/Blog");
+const BlogDetail = lazyComponent("../containers/BlogPage/BlogSingle");
+const AlmaaGroups = lazyComponent("../pages/About/AlmaaGroups/AlmaaGroups");
+const Founder = lazyComponent("../pages/About/Founder/Founder");
+const DoctorsTeam = lazyComponent("../pages/About/DoctorsTeam/DoctorsTeam");
+const DoctorDetail = lazyComponent("../pages/About/DoctorsTeam/DoctorDetail");
+const OurBranches = lazyComponent("../pages/About/OurBranches/OurBranches");
+const OurConcepts = lazyComponent("../pages/About/OurConcepts/OurConcepts");
+const SiddhaScience = lazyComponent(
+  "../pages/About/SiddhaScience/SiddhaScience"
+);
+const WellnessCenter = lazyComponent("../pages/WellnessCenter/WellnessCenter");
+const ProductionUnit = lazyComponent("../pages/ProductionUnit/ProductionUnit");
+const Library = lazyComponent("../pages/Library/Library");
+const CaseStudies = lazyComponent("../pages/Resources/CaseStudies/CaseStudies");
+const CaseStudyDetail = lazyComponent(
+  "../pages/Resources/CaseStudies/CaseStudyDetail"
+);
+const NewsAndEvents = lazyComponent(
+  "../pages/Resources/NewsAndEvents/NewsAndEvents"
+);
+const NewsAndEventsDetail = lazyComponent(
+  "../pages/Resources/NewsAndEvents/NewsAndEventsDetail"
+);
+const Media = lazyComponent("../pages/Resources/Media/Media");
+const Videos = lazyComponent("../pages/Resources/Videos/Videos");
+const PageAbout = lazyComponent("../containers/PageAbout/PageAbout");
+const PageSignUp = lazyComponent("../containers/PageSignUp/PageSignUp");
+const Login = lazyComponent("../pages/Login/Login");
+const ForgotPassword = lazyComponent("../pages/ForgotPassword/ForgotPassword");
+const Faq = lazyComponent("../pages/Faq");
+const Returns = lazyComponent("../pages/Returns/Returns");
+const TermsAndConditions = lazyComponent(
+  "../pages/TermsAndConditions/TermsAndConditions"
+);
+const PrivacyPolicy = lazyComponent("../pages/PrivacyPolicy/PrivacyPolicy");
+const ShippingPolicy = lazyComponent("../pages/ShippingPolicy/ShippingPolicy");
+const Invoice = lazyComponent("../components/Invoice/Invoice");
+const Support = lazyComponent("../pages/Support/Support");
+const Page404 = lazyComponent("../containers/Page404/Page404");
 
 export const pages: Page[] = [
   { path: "/", component: Home },
@@ -50,7 +70,7 @@ export const pages: Page[] = [
   { path: "/products/:categoryId/:category", component: Products },
   {
     path: "/product-detail/:name",
-    component: ProductDetailPage2,
+    component: ProductDetail,
   },
   { path: "/page-search", component: PageSearch },
   { path: "/account", component: AccountPage, protectedRoute: true },
@@ -75,7 +95,7 @@ export const pages: Page[] = [
   { path: "/checkout", component: CheckoutPage, protectedRoute: true },
   { path: "/thanks", component: ThanksPage, protectedRoute: true },
   { path: "/blog", component: Blog },
-  { path: "/blog/:id", component: BlogSingle },
+  { path: "/blog/:id", component: BlogDetail },
   { path: "/almaa-groups", component: AlmaaGroups },
   { path: "/founder", component: Founder },
   { path: "/doctors-team", component: DoctorsTeam },
